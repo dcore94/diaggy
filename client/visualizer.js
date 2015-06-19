6f16e223b10b7b8924390956fa3abe14f0dba124
dcore94 <marco.lettere@dedalus.eu>
2015-06-19 18:03:12 +0200

    partially added resizing feature

--- client/visualizer.js
+++ client/visualizer.js
@@ -36,11 +36,11 @@ function drag(evt){
 	var x = (evt['movementX'] ? evt.movementX : ( evt['mozMovementX'] ? evt['mozMovementX'] : 0))
 	var y = (evt['movementY'] ? evt.movementY : ( evt['mozMovementY'] ? evt['mozMovementY'] : 0))
 	move(dragged, x, y)
+	resizeparent(dragged)
 	drawwires()
 	evt.preventDefault(true)
 	return false;
 }
-
 
 function unselect(evt){
 	dragged.removeAttributeNS(null, "onmouseup")
@@ -56,16 +56,16 @@ var visualcache = {}
 function cache(ele){
 	var name = ele.getAttributeNS(null, 'name') 
 	var cacheentry = 
-			{ "left" : dragged.offsetLeft, "top" : dragged.offsetTop, "width" : dragged.offsetWidth, "height" : dragged.offsetHeight }
+			{ "left" : ele.offsetLeft, "top" : ele.offsetTop, "width" : ele.offsetWidth, "height" : ele.offsetHeight }
 	visualcache[name] = cacheentry
-	//console.log("added cache entry", name, cacheentry)
+	console.log("added cache entry", name, cacheentry)
 }
 
 function overridefromcache(ele){
 	var name = ele.getAttributeNS(null, 'name') 
 	if (visualcache[name]) {
 		var cached = visualcache[name]
-		//console.log("found in cache", name, cached[name])
+		console.log("found in cache", name, cached)
 		moveto(ele, cached['left'], cached['top'])
 		resizeto(ele, cached['width'], cached['height'])
 	}
@@ -86,7 +86,7 @@ function pathtocoords(path){
 		div = div.querySelector("div[name='" + path[i] + "']")
 		x += div.offsetLeft
 		y += div.offsetTop
-		console.log(div.getAttribute('name'), div.offsetLeft, div.offsetTop)
+		//console.log(div.getAttribute('name'), div.offsetLeft, div.offsetTop)
 	}
 	return [x, y]
 }
@@ -108,7 +108,7 @@ function drawwire(wire){
 	var prolog = source.getAttribute("prolog")
 	var epilog = target.getAttribute("epilog")
 	var d = origin + prolog + " L" + (co2[0] + ofsx) + "," + (co2[1] + ofsy) + epilog
-	console.log(ofsx, ofsy, d)
+	//console.log(ofsx, ofsy, d)
 	wire.setAttributeNS(null, "d", d)
 }
 
@@ -116,5 +116,17 @@ function drawwires(){
 	var wires = document.querySelectorAll(".wire")
 	for(var i=0; i < wires.length; i++){
 		drawwire(wires[i])
+	}
+}
+
+function resizeparent(ele){
+	var parent = ele.parentNode
+	if (parent.getAttribute("name") == "canvas") return;
+	var dw = Math.max(0, (ele.offsetLeft + ele.offsetWidth) - parent.offsetWidth) 
+	var dh = Math.max(0, (ele.offsetTop + ele.offsetHeight) - parent.offsetHeight)
+	if(dw != 0 || dh != 0){
+		console.log(parent, dw, dh)
+		resize(parent, dw, dh)
+		cache(parent)
 	}
 }
--- server/scaconstants.xqm
+++ server/scaconstants.xqm
@@ -3,14 +3,13 @@ module namespace _ = "urn:dcore:diaggy:constants:sca";
 declare variable $_:SPACING := 10;
 declare variable $_:INSETS := 20;
 
-declare variable $_:ARROW_LENGTH := 40;
-declare variable $_:ARROW_HEIGHT := 20;
+declare variable $_:ARROW_LENGTH := 60;
+declare variable $_:ARROW_HEIGHT := 30;
 declare variable $_:ARROW_LENGTH_HALF := $_:ARROW_LENGTH div 2;
 declare variable $_:ARROW_HEIGHT_HALF := $_:ARROW_HEIGHT div 2;
 declare variable $_:ARROW_OFFSET := $_:ARROW_LENGTH_HALF;
 declare variable $_:ARROW_OFFSET_NEG := - $_:ARROW_OFFSET;
-declare variable $_:ARROW_POINTS := "0,0 30,0 40,10 30,20 0,20 10,10";
-declare variable $_:ARROW_POINTS_NEG := "0,10 -10,0 -40,0 -30,10 -40,20 -10,20";
+declare variable $_:ARROW_POINTS := "0,0 50,0 60,15 50,30 0,30 10,15";
 declare variable $_:COMPOSITE_WIDTH := 300;
 declare variable $_:COMPOSITE_HEIGHT := 150;
 declare variable $_:COMPONENT_WIDTH := 150;
